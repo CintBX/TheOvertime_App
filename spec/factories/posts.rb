d214e1f20ded9994595c862c4749929db9d8ef30
Dustin Cintron <D.Cint88@gmail.com>
2019-01-30 16:46:41 -0500

    fixed operating system for git removed blank objects

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :post do
     date { Date.today }
-    rationale { "Some Rationale" }
+    rationale { "Some Rationale" }    
     user
   end
 
   factory :second_post, class: "Post" do
     date { Date.yesterday }
-    rationale { "Some more content" }
+    rationale { "Some more content" }    
     user
   end
 end
