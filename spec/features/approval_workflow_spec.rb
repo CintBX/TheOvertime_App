426511cc2285d04c79b05d62f0faee57a24ccc95
Dustin Cintron <D.Cint88@gmail.com>
2019-02-14 17:51:36 -0500

    Implemented ability to block users after post is marked approved

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -23,9 +23,22 @@ describe 'navigate' do
 			logout(:user)
 			user = FactoryBot.create(:user)
 			login_as(user, :scope => :user)
+
 			visit edit_post_path(@post)
 
 			expect(page).to_not have_content('Approved')
+		end
+
+		it 'should not be editable by the post creator if the status is approved' do
+			logout(:user)
+			user = FactoryBot.create(:user)
+			login_as(user, :scope => :user)
+
+			@post.update(user_id: user.id, status: 'approved')
+
+			visit edit_post_path(@post)
+			
+			expect(current_path).to eq(root_path)
 		end
 	end
 end
