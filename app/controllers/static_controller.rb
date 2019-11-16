616c42b00a2a1dbbb4c5ca29b5c1922ac964c231
Dustin Cintron <D.Cint88@gmail.com>
2019-11-16 00:25:04 -0500

    Curbed confirmation log for admins

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,7 +2,7 @@ class StaticController < ApplicationController
 	def homepage
 		if admin_types.include?(current_user.type)
 			@pending_approvals = Post.submitted.last(9)
-			@recent_audit_items = AuditLog.last(10)
+			@recent_audit_items = AuditLog.last(9)
 		else
 			@pending_audit_confirmations = current_user.audit_logs.pending.by_start_date
 		end
